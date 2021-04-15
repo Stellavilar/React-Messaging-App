ec0f3593be5528a436719f8e55428c87e4f3f400
Stella Vilar <vilar.stella@gmail.com>
2021-04-16 01:29:16 +0200

    Form built to create new contact

--- src/components/NewContactsModal.js
+++ src/components/NewContactsModal.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Modal, Form, Button } from 'react-bootstrap';
 
-export default function NewContactsModal() {
+export default function NewContactsModal({ closeModal }) {
+    const idRef = useRef();
+    const nameRef = useRef();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        // createContact(idRef.current.value, nameref.current.value)
+        closeModal();
+    }
+
     return (
-        <div>
-            new contact modal
-        </div>
+        <>
+            <Modal.Header closeButton>Ajouter un contact</Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={handleSubmit} >
+                    <Form.Group>
+                        <Form.Label>Id</Form.Label>
+                        <Form.Control type="text" ref={idRef} required />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Nom</Form.Label>
+                        <Form.Control type="text" ref={nameRef} required />
+                    </Form.Group>
+                    <Button type="submit">Sauvegarder</Button>
+                </Form>
+            </Modal.Body>
+        </>
     );
 };
