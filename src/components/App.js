cb016302dcc74d792d908e228005f9d4e126c929
Stella Vilar <vilar.stella@gmail.com>
2021-04-16 00:17:33 +0200

    Condition in App.js to be able to go on Dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import useLocalStorage from '../hooks/useLocalStorage';
 import Login from './Login';
+import Dashboard from './Dashboard';
 
 function App() {
   //Get new id thanks to useLocalStorage function
   const [ id, setId ] = useLocalStorage('id');
   return (
     <>
-      {id}
-      <Login getId={setId} />
+      {id ? <Dashboard /> : <Login getId={setId} /> }
     </>
   );
 };
