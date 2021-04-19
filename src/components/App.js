fe13ea0823d82170ca58f979f65a6901c25cc937
Stella Vilar <vilar.stella@gmail.com>
2021-04-19 14:03:00 +0200

    Install SocketProvider for the application to be connected to a socket

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,22 @@ import Login from './Login';
 import Dashboard from './Dashboard';
 import { ContactsProvider } from '../contexts/ContactsProvider';
 import { ConversationsProvider } from '../contexts/ConversationsProvider';
+import { SocketProvider } from '../contexts/SocketProvider';
 
 function App() {
   //Get new id thanks to useLocalStorage function
   const [ id, setId ] = useLocalStorage('id');
 
 const dashboard = (
-  //Dashboard has to be included in Provider to share some data and update it (contacts, conversations)
-  <ContactsProvider>
-    <ConversationsProvider>
-      <Dashboard id={id}/>
-    </ConversationsProvider>
-  </ContactsProvider>
+  //Dashboard has to be included in Provider to share some data and update it (contacts, conversations, socket)
+  <SocketProvider id={id}>
+    <ContactsProvider>
+      <ConversationsProvider>
+        <Dashboard id={id}/>
+      </ConversationsProvider>
+    </ContactsProvider>
+  </SocketProvider>
+  
 );
 
   return (
